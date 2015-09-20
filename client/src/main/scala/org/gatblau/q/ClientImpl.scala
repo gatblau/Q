befd986e68ce77797486de0a673d29599f454be7
gatblau <info@gatblau.org>
2015-09-20 20:20:53 +0100

    Caching and Tracking.

---
+++ client/src/main/scala/org/gatblau/q/ClientImpl.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 gatblau.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gatblau.q
+
+import org.gatblau.q.model.{Feature, FeatureSet, Catalogue}
+
+class ClientImpl extends Client {
+  override def isAvailable: Boolean = ???
+
+  override def createFeatureSet(featureSetData: FeatureSet): Long = ???
+
+  override def createCatalogue(catalogueData: Catalogue): Long = {
+    0
+  }
+
+  override def createFeature(featureData: Feature): Long = ???
+}
